dc487881302d134719b22ef68db49ef39a7859dd
Haller-x <34765767+Haller-x@users.noreply.github.com>
2020-02-01 15:19:46 -0300

    Add files via upload

---
+++ Sequencia collatz.py
@@ -0,0 +1,12 @@
+def collatz():
+    try:
+        nominal = int(input("Enter nominal"))
+    while nominal!=1:
+        if nominal%2==0:
+            nominal = nominal / 2
+            print(nominal)
+        else:
+            nominal=nominal * 3 +1
+            print(nominal)
+    except ValueError:
+        print("Digite um número inteiro")
